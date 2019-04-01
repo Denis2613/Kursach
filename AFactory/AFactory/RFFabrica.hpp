b78bf3c9cfbbcb5adc19654ed457e2a0b4f5529e
Denis2613 <uvade98@mail.ru>
2019-04-02 00:16:09 +0300

    Добавление проекта

---
+++ AFactory/AFactory/RFFabrica.hpp
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include "MilitaryVehicleFactory.hpp"
+
+ class RFFabrica: public MilitaryVehicleFactory{
+ public:
+	 virtual const char* Country(){ return "Russia"; };
+	 //выюираем тип перемещения
+	virtual Movement* CreateMovement(MovementType type){
+		if(type == MovementType::Fly){
+			return new FlyMovement();
+		}
+		else if(type == MovementType::Ride){
+			 return new RideMovement();
+		}
+		else if(type == MovementType::Swim){
+			 return new SwimMovement();
+		}
+		else{
+			return nullptr;
+		}
+	}
+	virtual Сharacteristic* CreateCharacteristic(std::istream& in){
+			Сharacteristic* c = new Сharacteristic();
+			in >> *c;
+			return c;
+	}
+ };
---
+++ AFactory/AFactory/main.cpp
@@ -0,0 +1,48 @@
+﻿#include "MilitaryVehicle.hpp"
+#include "USFabrica.hpp"
+#include "RFFabrica.hpp"
+
+#include <fstream>
+
+using namespace std;
+
+int main(){
+	setlocale(LC_ALL, "");
+   cout << "1. Из консоли\n2. Из фала\n";
+   MilitaryVehicle *mv = nullptr;
+   int a;
+   cin >> a;
+   if(a == 1){
+		cout << "1. США\n2. Россия\n";
+		cin >> a;
+		cout << "1. Летает\n2. Ездит\n3. Плавает\n";
+		int b;
+		cin >> b;
+		cout << "Кол-во расстояние экипаж мощность тип_оружия масса, год выпуска, тип техники:\n";
+		if(a == 1){
+			mv = new MilitaryVehicle(new USFabrica(), cin, (b == 0 ? MovementType::Fly : (b == 1 ? MovementType::Ride : MovementType::Swim)));
+		}
+		else{
+			mv = new MilitaryVehicle(new RFFabrica(), cin, (b == 0 ? MovementType::Fly : (b == 1 ? MovementType::Ride : MovementType::Swim)));
+		}
+   }
+   else{
+	      string fileName;
+		  cout << "File name: ";
+		  cin >> fileName;
+		  ifstream in(fileName);
+		  in >> a;
+		  int b;
+		  in >> b;
+		  if(a == 1){
+			mv = new MilitaryVehicle(new USFabrica(), in, (b == 0 ? MovementType::Fly : (b == 1 ? MovementType::Ride : MovementType::Swim)));
+		 }
+		 else{
+		 	mv = new MilitaryVehicle(new RFFabrica(), in, (b == 0 ? MovementType::Fly : (b == 1 ? MovementType::Ride : MovementType::Swim)));
+		 }
+   }
+   cout << '\n' << (*mv);
+   ofstream out("out.txt");
+   out << (*mv);
+   system("pause>>void");
+}
